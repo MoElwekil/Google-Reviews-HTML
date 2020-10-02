600f43b79d63aa7f84f1ea254e030dbf1f9e477e
Mohamed Elwekil <Mo@Descod.net>
2020-10-02 02:21:13 +0100

    reviews body first stage is ready

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+	$all = 			"https://www.google.com/search?q=cycle+legal+google+reviews&rlz=1C1CHBF_en-GBGB914GB914&oq=cycle+legal&aqs=chrome.0.69i59j46j0l3j69i60j69i61l2.7118j0j7&sourceid=chrome&ie=UTF-8#lrd=0x48761c9b3263933b:0x2c3d76dcad4a3e58,1,,,";
+	$outcome = 		"https://www.google.com/search?q=cycle+legal+google+reviews&rlz=1C1CHBF_en-GBGB914GB914&oq=cycle+legal&aqs=chrome.0.69i59j46j0l3j69i60j69i61l2.7118j0j7&sourceid=chrome&ie=UTF-8#lrd=0x48761c9b3263933b:0x2c3d76dcad4a3e58,1,,,&topic=mid:/m/0687b1";
+	$team = 		"https://www.google.com/search?q=cycle+legal+google+reviews&rlz=1C1CHBF_en-GBGB914GB914&oq=cycle+legal&aqs=chrome.0.69i59j46j0l3j69i60j69i61l2.7118j0j7&sourceid=chrome&ie=UTF-8#lrd=0x48761c9b3263933b:0x2c3d76dcad4a3e58,1,,,&topic=mid:/m/011l78";
+	$professional = "https://www.google.com/search?q=cycle+legal+google+reviews&rlz=1C1CHBF_en-GBGB914GB914&oq=cycle+legal&aqs=chrome.0.69i59j46j0l3j69i60j69i61l2.7118j0j7&sourceid=chrome&ie=UTF-8#lrd=0x48761c9b3263933b:0x2c3d76dcad4a3e58,1,,,&topic=mid:/m/01qkbx";
+	$bike = 		"https://www.google.com/search?q=cycle+legal+google+reviews&rlz=1C1CHBF_en-GBGB914GB914&oq=cycle+legal&aqs=chrome.0.69i59j46j0l3j69i60j69i61l2.7118j0j7&sourceid=chrome&ie=UTF-8#lrd=0x48761c9b3263933b:0x2c3d76dcad4a3e58,1,,,&topic=mid:/m/0199g";
+	$confidence = 	"https://www.google.com/search?q=cycle+legal+google+reviews&rlz=1C1CHBF_en-GBGB914GB914&oq=cycle+legal&aqs=chrome.0.69i59j46j0l3j69i60j69i61l2.7118j0j7&sourceid=chrome&ie=UTF-8#lrd=0x48761c9b3263933b:0x2c3d76dcad4a3e58,1,,,&topic=mid:/m/08h03n";
+	$case = 		"https://www.google.com/search?q=cycle+legal+google+reviews&rlz=1C1CHBF_en-GBGB914GB914&oq=cycle+legal&aqs=chrome.0.69i59j46j0l3j69i60j69i61l2.7118j0j7&sourceid=chrome&ie=UTF-8#lrd=0x48761c9b3263933b:0x2c3d76dcad4a3e58,1,,,&topic=mid:/m/05ps5_";
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -20,9 +30,326 @@
 			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
 		<![endif]-->
 
-	<h1>
-		Hello World!
-	</h1>
+	<div id="google_reviews">
+		<div class="inner float_left">
+			<div class="google_reviews_header float_left">
+				<div class="col_100 float_left">
+					<div class="col_50 float_left">
+						<div class="g_r_header">
+							<h1>Cycle Legal</h1>
+							<p class="g_r_address">11 Northwold Rd, London</p>
+						</div><!-- google reviews header -->
+					</div><!-- col 50 -->
+
+					<div class="col_50 float_left">
+						<div class="g_r_write_review">
+							<div class="pen_icon"></div>
+							<p class="display_inline_block">Write a review</p>
+						</div>
+					</div><!-- col 50 -->
+				</div><!-- col 100 -->
+
+				<div class="col_100 float_left g_r_rating">
+					<div class="col_50 float_left">
+						<p class="g_r_number 		display_inline_block">5.0</p>
+						<div class="g_r_full_star large 	display_inline_block"></div>
+						<div class="g_r_full_star large 	display_inline_block"></div>
+						<div class="g_r_full_star large 	display_inline_block"></div>
+						<div class="g_r_full_star large 	display_inline_block"></div>
+						<div class="g_r_full_star large 	display_inline_block"></div>
+						<p class="g_r_total_reviews display_inline_block">12 reviews</p>
+						<p class="g_r_question_icon display_inline_block"></p>
+					</div><!-- col 50 00 -->
+				</div><!-- col 100 -->
+
+				<div class="col_100 float_left">
+					<div class="g_r_list">
+						<ul>
+							<li><a href="<?php echo $all; ?>"			>All</a></li>
+							<li><a href="<?php echo $outcome; ?>"		>outcome</a></li>
+							<li><a href="<?php echo $team; ?>"			>team</a></li>
+							<li><a href="<?php echo $professional; ?>"	>professional</a></li>
+							<li><a href="<?php echo $bike; ?>"			>bike</a></li>
+							<li><a href="<?php echo $confidence; ?>"	>confidence</a></li>
+							<li><a href="<?php echo $case; ?>"			>case</a></li>
+						</ul>
+					</div>
+				</div>
+			</div><!-- google reviews header -->
+
+			<div class="google_reviews_body">
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh5.googleusercontent.com/-DyvqLPcKP5U/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnSpZ6PNDB09L84dgfAMTrNL_c9eA/s40-c0x00000000-cc-rp/photo.jpg" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">peter drinkell</div>
+							<div class="reviewer_count">2 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="reviewer_position"><b>Positive:</b> Professionalism, Responsiveness</div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>After my bike accident I needed to find a solicitor to handle my case. Searching the internet I came across ‘Cycle Legal’. Reading the reviews I decided to meet with Kevin to discuss my case. …<span class="more">More</span></p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh3.googleusercontent.com/a-/AOh14GiYXW-nnubvHJuSz1D_jN2n1jjrkmqYqUAtAnP37w=s40-c0x00000000-cc-rp" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Arabella Gibbins</div>
+							<div class="reviewer_count">7 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>Kevin and Cycle Legal provided me with great support throughout what was personally a long and difficult process. Kevin was recommended to me by a friend, and at the time I wasn't in any state to make decisions or research other avenues for …<span class="more">More</span></p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh6.googleusercontent.com/-AarQ8j3NdhA/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnYMlOyby80hoUJlsCdsNsivNye7Q/s40-c0x00000000-cc-rp/photo.jpg" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Angela Trujillo</div>
+							<div class="reviewer_count">1 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="reviewer_position"><b>Positive:</b> Professionalism</div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>Today we concluded the process following on from my cycling accident a couple of years ago.  Kevin and his team have been true professionals and it was clear from the very outset that Kevin has your best interest at heart.  Before …<span class="more">More</span></p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh3.googleusercontent.com/a-/AOh14GiZWjJLlavLBqzeWbp0RUHS4FFi3lebjEOyjUbY=s40-c0x00000000-cc-rp" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Marilyn Rust</div>
+							<div class="reviewer_count">1 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>I cannot recommend Kevin O'Sullivan and the staff at Cycle Legal highly enough. Being involved in an accident is traumatic enough without having to go through a personal injury claim but Kevin makes sure his clients are well supported …<span class="more">More</span></p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh3.googleusercontent.com/a-/AOh14GidbpJ0zrUQUFVG7oiMAtQpuM38r753XeimV7iI=s40-c0x00000000-cc-rp" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Hanna ter Meulen</div>
+							<div class="reviewer_count">1 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="reviewer_position"><b>Positive:</b> Professionalism, Quality, Responsiveness, Value</div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>Kevin O'Sullivan was extremely professional, honest, caring and kind in the handling of my cycle case, the eventual outcome of which was far better than I expected, or imagined. I'm very thankful and I can highly recommend him and his team.</p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh5.googleusercontent.com/-V2UVz9--DOU/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclhO7QYZiiWihy7GFiVj0lz8mFGuQ/s40-c0x00000000-cc-rp/photo.jpg" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Jeff Bass</div>
+							<div class="reviewer_count">1 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>We met with Kevin following my daughters accident and immediately felt very confident with him. He and the staff at Cycle Legal looked after her case for four years and kept us fully informed throughout. All the time their service was very …<span class="more">More</span></p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh3.googleusercontent.com/-8_vSIk853DY/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclVGoPK3sUXAsIJOvhCgorCfqD0qg/s40-c0x00000000-cc-rp/photo.jpg" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Hermione Comstock</div>
+							<div class="reviewer_count">1 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="reviewer_position"><b>Positive:</b> Professionalism, Quality, Responsiveness, Value</div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>Kevin O'Sullivan and his team at Cycle Legal have been fantastically thorough, transparent and kind, regarding a bike accident I had.  As a cyclist, Kevin clearly understands the frustrations we experience.  Communication is always in …<span class="more">More</span></p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh4.googleusercontent.com/-6dCamWn10BM/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmJMM6BZvAgC8afRjTaRwyGgVHKwA/s40-c0x00000000-cc-rp/photo.jpg" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Emma Leonard</div>
+							<div class="reviewer_count">1 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>Following a cycling accident, my sister (a solicitor) arranged for Cycle Legal to take on my case. I'm incredibly grateful for all that Kevin has done for me. He has been professional, personable and really thorough, keeping me updated throughout the process. The outcome was far better than expected. I highly recommend Cycle Legal.</p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh4.googleusercontent.com/-2PmjCkZPqM0/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuckmbg1j7zBAZ5fts5jwzuXGoqYM9A/s40-c0x00000000-cc-rp/photo.jpg" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">E Bloom</div>
+							<div class="reviewer_count">3 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>Kevin and his team at Cycle Legal were amazing and a pleasure to work with from start to finish. They dealt with my case with expertise, professionalism, compassion and patience – all qualities I admire. Kevin took the time to explain …<span class="more">More</span></p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh5.googleusercontent.com/-QMIPmw0Xj4E/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucmYhAh6AH4HsP4_Z5cVIG8MjIYLHg/s40-c0x00000000-cc-rp/photo.jpg" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Charlotte Bass</div>
+							<div class="reviewer_count">1 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>There is nothing I can fault about Cycle Legal.  After being introduced to Kevin following my accident he has always been a pleasure to work alongside.  Throughout the four and a half years of my litigation Kevin always gave me confidence, …<span class="more">More</span></p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh3.googleusercontent.com/a-/AOh14Gi6NhU6lXwzVWz3m14v5X0aKRRVK1fk3Wp5kpaD9g=s40-c0x00000000-cc-rp" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Robert Murtagh</div>
+							<div class="reviewer_count">2 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>I would highly recommend Cycle Legal. Kevin and the team were extremely helpful and worked really hard to ensure a good outcome for my case. In a sea of faceless no-win-no-fee firms, it was brilliant to find personable and knowledgable legal advice.</p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				<div class="g_r_comment">
+					<div class="g_r_comment_header">
+						<div class="reviewer_img float_left">
+							<img src="https://lh4.googleusercontent.com/-TVnqwyT8iEQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuckEWyxDMJESuStR6rjnLYLpsKqjDw/s40-c0x00000000-cc-rp/photo.jpg" alt="">
+						</div>
+
+						<div class="reviewer_details float_left width_minus_img">
+							<div class="reviewer_name">Robert Murtagh</div>
+							<div class="reviewer_count">3 review</div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+							<div class="g_r_full_star small display_inline_block"></div>
+						</div>
+					</div><!-- comment header -->
+
+					<div class="g_r_comment_body float_right width_minus_img">
+						<p>I found Kevin via Google and knew nothing about his company. When I met him he was super friendly, efficient and more interested in helping me than making money. Don't hesitate to make an appointment with him, I'm so glad I did!</p>
+					</div><!-- comment body -->
+				</div><!-- comment -->
+
+				
+			</div><!-- google reviews body -->
+		</div><!-- inner -->
+	</div><!-- google reviews -->
 
 </body>
 
